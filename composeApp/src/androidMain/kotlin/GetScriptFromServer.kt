da3894bbd74e184905c69a7e7b07c5360abf6986
Martinez <amartin5@th-koeln.de>
2024-02-19 18:09:10 +0100

    Remove IP Address Again
    Platzhalter sollte wie gehabt modifiziert werden

--- composeApp/src/androidMain/kotlin/GetScriptFromServer.kt
+++ composeApp/src/androidMain/kotlin/GetScriptFromServer.kt
@@ -8,7 +8,7 @@ private val client = HttpClient()
 
 actual suspend fun getLocationScriptFromServer(scriptName : String) : String {
     // Hier ip addresse von dem gerät wo der server drauf läuft :)
-    val ip = "192.168.178.112:3000"
+    val ip = "000.000.000.000:3000"
     val response = client.get("http://$ip/script/$scriptName")
     return response.bodyAsText()
 }
